42ba70bc4a1dc1c8aea0aa69251f42a8708c929d
LI-YONG-QI <a2369028@gmail.com>
2024-09-11 12:43:42 +0800

    test: add init assert

--- test/PoolManager.t.sol
+++ test/PoolManager.t.sol
@@ -5,6 +5,8 @@ import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
 import {PoolManager} from "v4-core/PoolManager.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {PoolId} from "v4-core/types/PoolId.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
 import {Currency} from "v4-core/types/Currency.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
@@ -14,36 +16,69 @@ import {console} from "forge-std/console.sol";
 
 contract PoolManagerTest is Test {
     using LPFeeLibrary for uint24;
+    using StateLibrary for IPoolManager;
 
-    uint160 public constant SQRT_PRICE_1_1 = 79228162514264337593543950336;
+    /// sqrtPriceX96 = floor(sqrt(A / B) * 2 ** 96) where A and B are the currency reserves
+    uint160 public constant SQRT_PRICE_1_1 = 79228162514264337593543950336; // A : B = 1 : 1;
     bytes constant ZERO_BYTES = new bytes(0);
+
     IPoolManager manager;
+    IHooks hooks = IHooks(address(0));
+    Currency c0;
+    Currency c1;
+
+    uint24 fee = 3000;
+    int24 tickSpacing = 60;
 
     function _initTokens() internal returns (Currency, Currency) {
         MockERC20 token0 = new MockERC20("TEST", "TEST", 18);
         MockERC20 token1 = new MockERC20("TEST", "TEST", 18);
 
-        Currency currency0 = Currency.wrap(address(token0));
-        Currency currency1 = Currency.wrap(address(token1));
+        c0 = Currency.wrap(address(token0));
+        c1 = Currency.wrap(address(token1));
 
-        return (currency0, currency1);
+        return (c0, c1);
+    }
+
+    function _getPoolKey() internal view returns (PoolKey memory key) {
+        key = PoolKey(c0, c1, fee, tickSpacing, hooks);
     }
 
     function setUp() public {
         manager = new PoolManager();
+        _initTokens();
     }
 
-    function test_initializePool() public {
-        (Currency currency0, Currency currency1) = _initTokens();
-        uint24 fee = 3000;
-        IHooks hooks = IHooks(address(0));
-        int24 tickSpacing = fee.isDynamicFee() ? int24(60) : int24(fee / 100 * 2);
-        PoolKey memory key = PoolKey(currency0, currency1, fee, tickSpacing, hooks);
-        manager.initialize(key, SQRT_PRICE_1_1, ZERO_BYTES);
+    function test_poolIdNotEq() public view {
+        PoolKey memory key = _getPoolKey();
         bytes32 id = PoolId.unwrap(key.toId());
 
-        PoolKey memory key2 = PoolKey(currency0, currency1, fee, tickSpacing, IHooks(address(1)));
+        PoolKey memory key2 = PoolKey(c0, c1, fee, tickSpacing, IHooks(address(1)));
         bytes32 id2 = PoolId.unwrap(key2.toId());
         assertNotEq(id, id2);
     }
+
+    function test_initializePool() public {
+        PoolKey memory key = _getPoolKey();
+
+        vm.expectEmit(true, true, true, true);
+        emit IPoolManager.Initialize(
+            key.toId(), c0, c1, fee, 60, hooks, SQRT_PRICE_1_1, TickMath.getTickAtSqrtPrice(SQRT_PRICE_1_1)
+        );
+        manager.initialize(key, SQRT_PRICE_1_1, ZERO_BYTES);
+
+        (uint160 sqrtPriceX96, int24 tick, uint24 protocolFee, uint24 lpFee) = manager.getSlot0(key.toId());
+        assertEq(sqrtPriceX96, SQRT_PRICE_1_1);
+        assertEq(tick, TickMath.getTickAtSqrtPrice(SQRT_PRICE_1_1));
+        assertEq(lpFee, fee);
+        assertEq(protocolFee, 0); //? How to determine the protocolFee
+    }
+
+    // function test_addLiquidity() public {
+    //     PoolKey memory key = _getPoolKey();
+    //     manager.initialize(key, SQRT_PRICE_1_1, ZERO_BYTES);
+
+    //     IPoolManager.ModifyLiquidityParams memory params = IPoolManager.ModifyLiquidityParams(0, 1, 1, bytes32(0));
+    //     manager.modifyLiquidity(key, params, ZERO_BYTES);
+    // }
 }
